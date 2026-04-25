f6b3582236b44ed0f69d35b37d0fb1d0934f0aa1
Calvin Richards <calvinrrichards64@gmail.com>
2026-04-25 14:15:11 -0700

    added relevant files to repository in Scripts/Levels

--- CMPM121Framework-main/Assets/Scripts/Movement/EnemyController.cs
+++ CMPM121Framework-main/Assets/Scripts/Movement/EnemyController.cs
@@ -8,7 +8,7 @@ public class EnemyController : MonoBehaviour
     public Hittable hp;
     public HealthBar healthui;
     public bool dead;
-
+    public float damage = 5f;
     public float last_attack;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -37,7 +37,7 @@ public class EnemyController : MonoBehaviour
         if (last_attack + 2 < Time.time)
         {
             last_attack = Time.time;
-            target.gameObject.GetComponent<PlayerController>().hp.Damage(new Damage(5, Damage.Type.PHYSICAL));
+            target.gameObject.GetComponent<PlayerController>().hp.Damage(new Damage(damage, Damage.Type.PHYSICAL));
         }
     }
 
